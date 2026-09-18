365b60aa0a83dc093bb97612047557ab89147220
Taylan Gökkaya <insomnimus@proton.me>
2026-09-18 12:13:52 +0300

    perf: increased pot sensitivty

--- firmware/src/tasks/adc.rs
+++ firmware/src/tasks/adc.rs
@@ -29,7 +29,7 @@ const SAMPLING_RATE: i32 = 2000;
 const ADC_AVERAGE_WINDOW: usize = 8;
 const SAMPLE_PERIOD_US: u64 = 1_000_000 / SAMPLING_RATE as u64;
 const ADC_INPUT_RANGE: (i32, i32) = (10, 4085);
-const MOVEMENT_THRESHOLD: i32 = 50;
+const MOVEMENT_THRESHOLD: i32 = 20;
 
 #[allow(nonstandard_style)]
 pub struct AdcPins {
